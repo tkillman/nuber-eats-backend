d4c149131cddecbb5c2384a84e049e0a2578dd17
kimdonggyu <dkkim1004@hanmail.net>
2024-12-23 00:33:04 +0900

    Update Restaurant part Two

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -2,6 +2,7 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { RestaurantsService } from './restaurants.service';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 
 @Resolver(() => Restaurant)
 export class ResutaurantsResolver {
@@ -28,5 +29,21 @@ export class ResutaurantsResolver {
       console.log(error);
       return false;
     }
+  }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args() updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<boolean> {
+    console.log(updateRestaurantDto);
+
+    try {
+      await this.restaurantsService.updateRestaurant(updateRestaurantDto);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+    return true;
   }
 }
