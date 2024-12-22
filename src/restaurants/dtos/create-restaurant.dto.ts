b161495253227e0cb06d6ed60be44f50e5a88cff
kimdonggyu <dkkim1004@hanmail.net>
2024-12-23 00:01:38 +0900

    Mapped Types

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,23 +1,33 @@
-import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { ArgsType, Field, InputType, OmitType } from '@nestjs/graphql';
 import { IsBoolean, IsString, Length } from 'class-validator';
+import { Restaurant } from '../entities/restaurants.entity';
 
 // @InputType() inputType을 쓰면 객체명을 명시해야함, ArgsType은 객체가 풀어짐
-@ArgsType()
-export class CreateRestaurantDto {
-  @Field(() => String)
-  @IsString()
-  name: string;
+// @ArgsType()
+// export class CreateRestaurantDto {
+//   @Field(() => String)
+//   @IsString()
+//   name: string;
 
-  @Field(() => Boolean)
-  @IsBoolean()
-  isVegan: boolean;
+//   @Field(() => Boolean)
+//   @IsBoolean()
+//   isVegan: boolean;
 
-  @Field(() => String)
-  @IsString()
-  address: string;
+//   @Field(() => String)
+//   @IsString()
+//   address: string;
 
-  @Field(() => String)
-  @IsString()
-  @Length(5, 10)
-  ownerName: string;
-}
+//   @Field(() => String)
+//   @IsString()
+//   @Length(5, 10)
+//   ownerName: string;
+// }
+
+// 매번 Restaurant의 변화를 감지할 수 없으니까 이렇게 사용
+
+@InputType()
+export class CreateRestaurantDto extends OmitType(
+  Restaurant,
+  ['id'],
+  InputType,
+) {}
--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 // ObjectType, Field 는 그래프큐엘용도
@@ -13,21 +14,27 @@ export class Restaurant {
 
   @Field(() => String)
   @Column()
+  @IsString()
   name: string;
 
   @Field(() => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field(() => String)
   @Column()
+  @IsString()
+  @Length(5, 10)
   ownerName: string;
 
   @Field(() => String)
   @Column()
+  @IsString()
   categoryName: string;
 }
