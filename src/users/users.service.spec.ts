ca57c122b248a1a417884a4c80748c8e5aea18aa
kimdonggyu <dkkim1004@hanmail.net>
2024-12-31 11:51:09 +0900

    Mocking part Two

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -5,6 +5,7 @@ import { User } from './entites/user.entity';
 import { Verification } from './entites/verification.entity';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   find: jest.fn(),
@@ -23,8 +24,12 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('UserService', () => {
   let service: UsersService;
+  //let usersRepository: Partial<Record<'hello', number>>;
+  let usersRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -50,6 +55,7 @@ describe('UserService', () => {
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('정의됐는지 확인', () => {
