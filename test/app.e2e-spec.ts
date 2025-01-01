31dea684341f2f1c278c3674179cb0d6f5fa7504
kimdonggyu <dkkim1004@hanmail.net>
2025-01-01 13:54:55 +0900

    Testing createAccount part Two

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,12 +2,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { getConnection } from 'typeorm';
+import { DataSource, getConnection } from 'typeorm';
+
+jest.mock('axios');
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let dataSource: DataSource; /* 추가된 코드 */
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,11 +18,12 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = module.createNestApplication();
+    dataSource = module.get(DataSource); /* 추가된 코드 */
     await app.init();
   });
 
   afterAll(async () => {
-    //await getConnection().dropDatabase();
+    await dataSource?.dropDatabase(); /* 추가된 코드 */
     await app.close();
   });
 
@@ -47,6 +51,30 @@ describe('AppController (e2e)', () => {
         .expect((res) => {
           console.log('🚀 ~ .expect ~ res:', res.body.data);
           expect(res.body.data.createUser.ok).toEqual(true);
+        });
+    });
+
+    it('[실패] 유저 중복', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createUser(input : {
+                email : "${email}",
+                password : "1234",
+                role : Client
+              }){
+                error
+                ok
+              }
+            }
+        `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createUser.ok).toEqual(false);
+          expect(res.body.data.createUser.error).toEqual(expect.any(String));
         });
     });
   });
