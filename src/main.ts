9c961888e49a30cbaba0430a39bc9e9070d7de99
kimdonggyu <dkkim1004@hanmail.net>
2025-01-14 19:38:57 +0900

    cors 허용

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({}));
   //app.use(jwtMiddleware);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors();
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
